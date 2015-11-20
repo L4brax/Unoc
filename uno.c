f01d68075f5bcfb6b6da42ea3243106f3d0d115d
L4brax <ant.hess2@gmail.com>
2015-11-20 15:48:20 +0100

    Ajouts fonctions et main.

--- uno.c
+++ uno.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // ************************************
 // Définition des types : liste chainée 
@@ -21,6 +22,7 @@ typedef struct _TCelluleCarte
 
 typedef struct _TJoueur
 {
+	int noJ; // Identifie le joueur.
 	char nom[15]; //Nom du joueur attribué en début de partie.
 	int nbCartes; // Nombre de carte en main du joueur.
 	TCelluleCarte* jmain; //Main du joueur.
@@ -31,6 +33,7 @@ TCelluleCarte* pioche; // Pioche principale du jeu. Gérer comme une pile.
 TCelluleCarte* jeu; // Pile de TCelluleCarte sur laquelle les joueurs ajoutent les cartes qu'ils peuvent poser.
 					// Cette pile est ensuite mélangée et placée dans la pioche si plus de carte dans la pioche.
 
+
 // ************************
 // Prototypes et fonctions 
 // ************************
@@ -40,17 +43,49 @@ int verifierCouleur(TCarte carteJoueur,TCarte carteJeu); 	// Vérifie si les cou
 															// Renvoi 1 si vrai, 0 si faux.
 int verifierValeur(TCarte,TCarte); 	// Vérifie si les valeurs des deux cartes en parametres sont identiques.
 									// Renvoi 1 si vrai, 0 si faux.
+void modifierNomJoueur(TJoueur* joueur); // Permet de changer le nom du joueur en paramètre.
+void afficherNomJ(TJoueur joueur); // affiche le nom du joueur en paramètre.
+void initJoueur(TJoueur* joueur); // Initialise le joueur.
+
 // ********************
 // Programme principal
 // ********************
 
 int main()
 {
-	printf("===========================================")
-    printf("=====    Prêt pour une nouvelle     =======")
-    printf("=====           de UNO !            =======")
-    printf("===========================================")
-
+	// Lexique
+	int choixMenu;	// Variable de choix du menu.
+	int end = 0;	// Variable utilisée pour mettre fin au programme dans la boucle principale.
+	TJoueur* jtour; // Pointeur indiquant le joueur devant jouer une carte ou piocher.
+	 
+	// Initialisation
+	TJoueur j1;
+	TJoueur j2;
+	TJoueur j3;
+	initJoueur(&j1);
+	initJoueur(&j2);
+	initJoueur(&j3);
+	jtour = &j1; // On positionne le pointeur du tour sur le premier joueur.
+	
+	printf("================================================================\n");
+    printf("===============      Prêt pour une nouvelle     ================\n");
+    printf("===============          partie de UNO !        ================\n");
+    printf("================================================================\n\n");
+    
+    while (end != 1) {
+		printf("----------------------------------------------------------------\n");
+		printf("---------------         Menu principal          ----------------\n");
+		printf("----------------------------------------------------------------\n");
+		printf(" 1 - Nouvelle partie \n");
+		if (strcmp(j1.nom," ")!=0) {  // Si une partie a déjà été jouée
+			printf(" 2 - Changer les noms des joueurs\n");
+		}
+		printf(" 0 - Quiter\n");
+		printf("----------------------------------------------------------------\n\n");
+		
+		scanf("%d", &choixMenu);
+		modifierNomJoueur(&j1);
+	}
 	return 0;
 }
 
@@ -74,8 +109,33 @@ int verifierValeur(TCarte carteJoueur, TCarte carteJeu){
 	return 1;
 }
 	
+void modifierNomJoueur(TJoueur* joueur){
+	char numJoueur[9]; // Chaine de caractère identifiant le joueur 
+	char nomTemp[15]; // Chaine de caractère tempon pour stocker le nom entré par l'utilisateur.
 	
-	
+	if (joueur->noJ == 1){
+		strcpy(numJoueur, "premier");
+	} else if (joueur->noJ == 2) {
+		strcpy(numJoueur, "second");
+	} else {
+		strcpy(numJoueur, "troisieme");
+	}
+	printf("Veuillez indiquer le nom du %s joueur : ",numJoueur);
+	scanf("%s", nomTemp);
+	strcpy(joueur->nom, nomTemp);
+}
+
+void afficherNomJ(TJoueur joueur){
+	printf("%s \n",joueur.nom);
+}
+
+void initJoueur(TJoueur* joueur){
+	joueur->jmain = NULL;
+	joueur->jsuivant = NULL;
+	joueur->nbCartes = 0;
+	joueur->noJ = 0;
+	strcpy(joueur->nom," ");
+}
 	
 	
 	
