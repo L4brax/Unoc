a1255d382617b66beec0a66cb15e44e2584554cf
L4brax <ant_hess2@gmail.com>
2015-11-27 13:36:42 +0100

    Fonctions + main

--- uno.c
+++ uno.c
@@ -25,8 +25,10 @@ typedef struct _TJoueur
 	int noJ; // Identifie le joueur.
 	char nom[15]; //Nom du joueur attribué en début de partie.
 	int nbCartes; // Nombre de carte en main du joueur.
+	int nbPoints; // Score du joueur.
 	TCelluleCarte* jmain; //Main du joueur.
 	struct _TJoueur* jsuivant; //Pointeur permettant de passer au joueur suivant une fois le tour terminé.
+
 } TJoueur;
 
 TCelluleCarte* pioche; // Pioche principale du jeu. Gérer comme une pile.
@@ -46,12 +48,20 @@ int verifierValeur(TCarte,TCarte); 	// Vérifie si les valeurs des deux cartes e
 void modifierNomJoueur(TJoueur* joueur); // Permet de changer le nom du joueur en paramètre.
 void afficherNomJ(TJoueur joueur); // affiche le nom du joueur en paramètre.
 void initJoueur(TJoueur* joueur); // Initialise le joueur.
-int affichageMenu(TJoueur joueur);	// Affiche le menu principal et demande l'action à executer.
+int affichageMenu(TJoueur joueur, int nbManches);	// Affiche le menu principal et demande l'action à executer.
 									// Retoune le choix de l'action à exectuer sous forme d'entier.
 									// joueur1 en paramère est utilisé pour savoir si il sagit de la première partie ou non.
-int nouvellePartie(TJoueur joueur)	// Retourne 1 si les noms des joueurs ne sont pas définit,
+									// nbManches en paramètre permet de savoir si une partie est en cours ou non.
+int nouveauJeu(TJoueur joueur);	// Retourne 1 si les noms des joueurs ne sont pas définit,
 									// signifiant qu'aucune partie n'a été déjà jouée.
-									// Le paramètre utilise le nom du joueur.
+									// Le paramètre utilise le nom d'un joueur, en général le premier.
+void jouerTour(TJoueur* joueur);
+void afficherUno();				// Affiche UNO pour indiquer que le joueur en Cours à gagné.
+void compterPoints(TJoueur* joueur); 	// Compte les points de la main du joueur en paramètre et les ajoutes sont total de points.
+										// Affiche aussi les points de la manche et de la partie en cours.
+void afficherResultat(TJoueur joueur); // Affiche le résultat des points du joueur en paramètre pour cette manche et pour la partie.
+void afficherVainqueur(TJoueur joueur1, TJoueur joueur2, TJoueur joueur3);  // Affiche le vainqueur 
+
 
 
 // ********************
@@ -61,10 +71,10 @@ int nouvellePartie(TJoueur joueur)	// Retourne 1 si les noms des joueurs ne sont
 int main()
 {
 	// Lexique
-	int choixMenu;	// Variable de choix du menu.
-	int end = 0;	// Variable utilisée pour mettre fin au programme dans la boucle principale.
+	int choixMenu = 1;	// Variable de choix du menu. Initialisée à 1 pour entrer dans la première boucle.
+	int finManche = 0;	// Variable utilisée pour mettre fin à la manche en cours.
 	TJoueur* jtour; // Pointeur indiquant le joueur devant jouer une carte ou piocher.
-	//int nbParties = 0;
+	int nbManches = 0; // Utilisé pour compter le nombre de manches jouées.
 	 
 	// Initialisation
 	TJoueur j1;
@@ -73,10 +83,13 @@ int main()
 	initJoueur(&j1);
 	initJoueur(&j2);
 	initJoueur(&j3);
+	j1.jsuivant = &j2;
+	j2.jsuivant = &j3;
+	j3.jsuivant = &j1;
 	j1.noJ = 1;
 	j2.noJ = 2;
 	j3.noJ = 3;
-	//jtour = &j1; // On positionne le pointeur du tour sur le premier joueur.
+	jtour = &j1; // On positionne le pointeur du tour sur le premier joueur.
 	
 	printf("================================================================\n");
     printf("===============      Prêt pour une nouvelle     ================\n");
@@ -84,14 +97,34 @@ int main()
     printf("================================================================\n\n");
     
     while (choixMenu != 0) {
-		choixMenu = affichageMenu(j1);
+		choixMenu = affichageMenu(j1,nbManches);
 
-		if (choixMenu == 1) {
-			if (nouvellePartie(j1)==1){
-				modifierNomJoueur(j1);
-				modifierNomJoueur(j2);
-				modifierNomJoueur(j3);
-
+		if ((choixMenu == 1) || (choixMenu == 2)) {
+			if (choixMenu == 1){
+				nbManches = 0;
+			}
+			if (nouveauJeu(j1)==1){
+				printf("----------------------------------------------------------------\n");
+				modifierNomJoueur(&j1);
+				modifierNomJoueur(&j2);
+				modifierNomJoueur(&j3);
+				printf("----------------------------------------------------------------\n");
+			}
+			while (finManche == 0) {
+				printf("%s à vous de jouer !\n",jtour->nom);
+				jouerTour(jtour);
+				if (jtour->jmain == NULL) {
+					afficherUno();
+					finManche = 1;
+					compterPoints(&j1);
+					compterPoints(&j2);
+					compterPoints(&j3);
+					nbManches =+ 1;
+					if (nbManches == 3) {
+						afficherVainqueur(j1,j2,j3);
+					}
+				}
+				jtour = jtour->jsuivant;
 			}
 		}		
 	}
@@ -129,9 +162,15 @@ void modifierNomJoueur(TJoueur* joueur){
 	} else {
 		strcpy(numJoueur, "troisieme");
 	}
-	printf("Veuillez indiquer le nom du %s joueur : ",numJoueur);
-	scanf("%s", nomTemp);
-	strcpy(joueur->nom, nomTemp);
+	do {
+		printf("Veuillez indiquer le nom du %s joueur : ",numJoueur);
+		scanf("%s", nomTemp);
+		if (strcmp(nomTemp," ")==0){
+			printf("Veuillez saisir au moins un caractère.");
+		} else {
+			strcpy(joueur->nom, nomTemp);
+		}
+	} while (strcmp(nomTemp," ")==0);
 }
 
 void afficherNomJ(TJoueur joueur){
@@ -143,25 +182,32 @@ void initJoueur(TJoueur* joueur){
 	joueur->jsuivant = NULL;
 	joueur->nbCartes = 0;
 	joueur->noJ = 0;
-	strcpy(joueur->nom,"ok");
+	strcpy(joueur->nom," ");
 }
 	
-int affichageMenu(TJoueur joueur) {
+int affichageMenu(TJoueur joueur, int nbManches) {
 	int choixMenu;
 	printf("----------------------------------------------------------------\n");
 	printf("---------------         Menu principal          ----------------\n");
 	printf("----------------------------------------------------------------\n");
 	printf(" 1 - Nouvelle partie \n");
-	if (nouvellePartie(joueur)==1) {  // Si une partie a déjà été jouée, les joueurs ont déjà été déclarés.
+	if (nouveauJeu(joueur)==1) {  // Si une partie a déjà été jouée, les joueurs ont déjà été déclarés.
 		printf(" 2 - Changer les noms des joueurs\n");
 	}
+	if (nbManches > 0) {
+		printf(" 3 - CONTINUER LA PARTIE EN COURS\n");
+	}
 	printf(" 0 - Quitter\n");
-	printf("----------------------------------------------------------------\n\n");
+	printf("----------------------------------------------------------------\n");
 	
-	if (nouvellePartie(joueur)==1){
+	if (nouveauJeu(joueur)==1){
 		do {
 			scanf("%d", &choixMenu);
 		} while ((choixMenu<0) || (choixMenu>1));
+	} else if (nbManches>0){
+		do {
+			scanf("%d", &choixMenu);
+		} while ((choixMenu<0) || (choixMenu>3));
 	} else {
 		do {
 			scanf("%d", &choixMenu);
@@ -171,17 +217,26 @@ int affichageMenu(TJoueur joueur) {
 	return choixMenu;
 }
 	
-int nouvellePartie(TJoueur joueur){
+int nouveauJeu(TJoueur joueur){
 	if (strcmp(joueur.nom," ")==0){
 		return 1;
 	} else {
 		return 0;
 	}
 }
-	
-	
-	
-	
-	
-	
-	
+
+void jouerTour(TJoueur* joueur){
+
+}
+
+void afficherUno(){
+
+}
+
+void compterPoints(TJoueur* joueur) {
+
+}
+
+void afficherVainqueur(TJoueur joueur1, TJoueur joueur2, TJoueur joueur3){
+
+}
