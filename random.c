af2973580811a0234a2c0e9a9e948f73deb9d5cb
L4brax <ant_hess2@gmail.com>
2015-12-11 02:12:19 +0100

    dhkshi

--- random.c
+++ random.c
@@ -36,8 +36,32 @@ int main(){
 }
 
 void initialiserJeu(){
-	ajoutSerie(1,10,"Bl",1,1);
-	ajoutSerie(13,14,"Sa",6,9);
+	ajoutSerie(1,10,"Bl",1,2);
+	ajoutSerie(1,10,"Ro",1,2);
+	ajoutSerie(1,10,"Ja",1,2);
+	ajoutSerie(1,10,"Ve",1,2);
+
+	ajoutSerie(20,21,"Bl",2,2);
+	ajoutSerie(20,21,"Ro",2,2);
+	ajoutSerie(20,21,"Ja",2,2);
+	ajoutSerie(20,21,"Ve",2,2);
+
+	ajoutSerie(20,21,"Bl",3,2);
+	ajoutSerie(20,21,"Ro",3,2);
+	ajoutSerie(20,21,"Ja",3,2);
+	ajoutSerie(20,21,"Ve",3,2);
+
+	ajoutSerie(20,21,"Bl",4,2);
+	ajoutSerie(20,21,"Ro",4,2);
+	ajoutSerie(20,21,"Ja",4,2);
+	ajoutSerie(20,21,"Ve",4,2);
+
+	ajoutSerie(50,51,"Sa",5,4);
+	ajoutSerie(50,51,"Sa",6,4);
+	ajoutSerie(0,1,"Ve",0,1);
+	ajoutSerie(0,1,"Ve",0,1);
+	ajoutSerie(0,1,"Ve",0,1);
+	ajoutSerie(0,1,"Ve",0,1);
 }
 
 void ajoutSerie(int a, int b, char couleur[], int type, int nb){
@@ -140,22 +164,11 @@ void afficherSuperListe(){
 			}
 			aux = aux->suivant;
 		}
-
 		printf("%s\n",string);
-<<<<<<< HEAD
-		printf("   ----   ----   ----   ----   ----   ----   ----   ----\n\n");
-		strcpy(string,"");
-		aux = jeu;
-		for (y=0;y<i*8;y++){
-			aux=aux->suivant;
-		}
-=======
 		printf("  *--*  *--*  *--*  *--*  *--*  *--*  *--*  *--*\n");
-
->>>>>>> d5aab27ef973da59339a7f8edba2efd5229b3735
 	}
 
-	/*char string1[56];
+	char string1[56];
 	char string2[56];
 	char string3[56];
 	char string4[56];
@@ -202,12 +215,38 @@ void afficherSuperListe(){
 		printf("%s\n",string3);
 		printf("%s\n\n",string4);
 	}
-<<<<<<< HEAD
-	printf("%s\n",string1);
-	printf("%s\n",string2);
-	printf("%s\n",string3);
-	printf("%s\n\n",string4);*/
 }
-=======
+
+void melangerPioche(){
+	TCelluleCarte* aux = jeu;
+	TCelluleCarte* prec; 
+	int i =0;
+	int y =0;
+	int select;
+	int tailleFile = 0;
+
+	while (aux != NULL) {
+		tailleFile += 1;
+		aux=aux->suivant;
+	}
+
+	for (i=tailleFile;i>0;i--){
+		select = rand()%i;
+		aux=jeu;
+		prec=jeu;
+		if (select==0) {
+			jeu = aux->suivant;
+			aux->suivant=pioche;
+			pioche=aux;
+		} else {
+			for (y=0;y<select;y++){
+				prec=aux;
+				aux=aux->suivant;
+			}
+			prec->suivant = aux->suivant;
+			aux->suivant = pioche;
+			pioche = aux;
+		}
+	}
 }
->>>>>>> d5aab27ef973da59339a7f8edba2efd5229b3735
+
--- uno.c
+++ uno.c
@@ -39,8 +39,8 @@ typedef struct _TJoueur
 
 } TJoueur;
 
-TCelluleCarte* pioche; // Pioche principale du jeu. Gérer comme une pile.
-TCelluleCarte* jeu; // Pile de TCelluleCarte sur laquelle les joueurs ajoutent les cartes qu'ils peuvent poser.
+TCelluleCarte* pioche = NULL; // Pioche principale du jeu. Gérer comme une pile.
+TCelluleCarte* jeu = NULL; // Pile de TCelluleCarte sur laquelle les joueurs ajoutent les cartes qu'ils peuvent poser.
 					// Cette pile est ensuite mélangée et placée dans la pioche si plus de carte dans la pioche.
 
 
@@ -56,7 +56,7 @@ int verifierValeur(TCarte,TCarte); 	// Vérifie si les valeurs des deux cartes e
 void modifierNomJoueur(TJoueur* joueur); // Permet de changer le nom du joueur en paramètre.
 void afficherNomJ(TJoueur joueur); // affiche le nom du joueur en paramètre.
 void initJoueur(TJoueur* joueur); // Initialise le joueur.
-int affichageMenu(TJoueur joueur, int nbManches);	// Affiche le menu principal et demande l'action à executer.
+int affichageMenu(int nbManches);	// Affiche le menu principal et demande l'action à executer.
 									// Retoune le choix de l'action à exectuer sous forme d'entier.
 									// joueur1 en paramère est utilisé pour savoir si il sagit de la première partie ou non.
 									// nbManches en paramètre permet de savoir si une partie est en cours ou non.
@@ -71,6 +71,17 @@ void compterPoints(TJoueur* joueur); 	// Compte les points de la main du joueur 
 void afficherResultat(TJoueur joueur); // Affiche le résultat des points du joueur en paramètre pour cette manche et pour la partie.
 void afficherVainqueur(TJoueur joueur1, TJoueur joueur2, TJoueur joueur3);  // Affiche le vainqueur 
 int saisirEntre(int a, int b); 	// Demande un entier à un utilisateur entre a et b et le retourne.
+void initialiserJeu(); //Initialise toutes les cartes du jeu dans la pile Jeu non mélangées.
+void ajoutSerie(int a, int b, char couleur[2], int type, int nb); // Permet d'ajouter une série de cartes à la pile Jeu.
+						// Le paramètre a désigne la borne inférieur de l'itération de la valeur de la carte (a inclus)
+						// Le paramètre b désigne la borne supérieur de l'itération de la valeur de la carte (b exclus)
+						// Couleur désigne la couleur affectée aux cartes créées.
+						// Type désigne le type affecté aux cartes crées.
+						// nb désigne le nombre de séries à créer.
+void afficherListe(*TCelluleCarte liste); 	//Affiche une liste de Carte.
+											//Prend en paramètre un pointeur vers une TCelluleCarte, en générale une main.
+											//Affiche plusieurs cartes sur une seule ligne.
+void melangerPioche(); //Mélange les cartes du jeu dans la pioche.
 
 
 
@@ -99,7 +110,7 @@ int main()
 	j1.noJ = 1;
 	j2.noJ = 2;
 	j3.noJ = 3;
-	jtour = &j1; // On positionne le pointeur du tour sur le premier joueur.
+	
 	
 	printf("================================================================\n");
     printf("===============      Prêt pour une nouvelle     ================\n");
@@ -107,13 +118,11 @@ int main()
     printf("================================================================\n\n");
     
     while (choixMenu != 0) {
-		choixMenu = affichageMenu(j1,nbManches);
-
-		if ((choixMenu == 1) || (choixMenu == 2)) {
-			if (choixMenu == 1){
+		choixMenu = affichageMenu(j1,nbManches);		
+		if ((choixMenu == 1) || (choixMenu == 2)){
+			jtour = &j1; // On positionne le pointeur du tour sur le premier joueur.
+			if (choixMenu == 1) {
 				nbManches = 0;
-			}
-			if (nouveauJeu(j1)==1){
 				printf("----------------------------------------------------------------\n");
 				modifierNomJoueur(&j1);
 				modifierNomJoueur(&j2);
@@ -173,7 +182,6 @@ void modifierNomJoueur(TJoueur* joueur){
 		strcpy(numJoueur, "troisieme");
 	}
 	do {
-		
 		printf("Veuillez indiquer le nom du %s joueur : ",numJoueur);
 		scanf("%s", nomTemp);
 		if (strcmp(nomTemp," ")==0){
@@ -196,27 +204,22 @@ void initJoueur(TJoueur* joueur){
 	strcpy(joueur->nom," ");
 }
 	
-int affichageMenu(TJoueur joueur, int nbManches) {
+int affichageMenu(int nbManches) {
 	int choixMenu;
 	printf("----------------------------------------------------------------\n");
 	printf("---------------         Menu principal          ----------------\n");
 	printf("----------------------------------------------------------------\n");
 	printf(" 1 - Nouvelle partie \n");
-	if (nouveauJeu(joueur)==1) {  // Si une partie a déjà été jouée, les joueurs ont déjà été déclarés.
-		printf(" 2 - Changer les noms des joueurs\n");
-	}
-	if (nbManches > 0) {
-		printf(" 3 - CONTINUER LA PARTIE EN COURS\n");
+	if (nbManches != 0) {  //Si une partie a déjà été jouée, le nombre de manche est différent de 0.
+		printf(" 2 - CONTINUER LA PARTIE EN COURS\n");
 	}
 	printf(" 0 - Quitter\n");
 	printf("----------------------------------------------------------------\n");
 	
-	if (nouveauJeu(joueur)==1){
-		choixMenu = saisirEntre(0,1);
-	} else if (nbManches>0){
-		choixMenu = saisirEntre(0,3);
-	} else {
+	if (nbManches!=0){
 		choixMenu = saisirEntre(0,2);
+	} else {
+		choixMenu = saisirEntre(0,1);
 	}
 
 	return choixMenu;
@@ -243,6 +246,13 @@ void jouerTour(TJoueur* joueur){
 }
 
 void afficherUno(){
+	printf("================================================================\n");
+	printf("===============     #    #  #   #   ###      ===================\n");
+	printf("===============     #    #  ##  #  #   #     ===================\n");
+	printf("===============     #    #  # # #  #   #     ===================\n");
+	printf("===============     #    #  #  ##  #   #     ===================\n");
+	printf("===============      ####   #   #   ###      ===================\n");
+	printf("================================================================\n");
 
 }
 
@@ -254,5 +264,204 @@ void afficherVainqueur(TJoueur joueur1, TJoueur joueur2, TJoueur joueur3){
 
 }
 
+void afficherListe(*TCelluleCarte liste){
+	TCelluleCarte* aux = liste;
+	int tailleFile = 0;
+	int nbLignes = 0;
+	
+	while (aux != NULL) {
+		tailleFile += 1;
+		aux=aux->suivant;
+	}
 
+	nbLignes = tailleFile/8; //8 car je désire afficher 8 cartes par ligne.
 
+	int i=0;
+	int y=0;
+	char string[56];
+	char stringtemp[7];
+	strcpy(string,"");
+	strcpy(stringtemp,"");
+
+	//Dans cette boucle on affiche les lignes complètes.
+	for (i=0;i<nbLignes;i++){ // On itère pour chaque ligne
+		
+		strcpy(string,"");
+		aux = liste;
+		for (y=0;y<i*8;y++){	// à chaque ligne, il faut replacer le pointeur sur la bonne carte afin d'obtenir les bonnes infos
+			aux=aux->suivant;
+		}
+		
+		printf("  .--.  .--.  .--.  .--.  .--.  .--.  .--.  .--.\n");
+		for (y=0;y<8;y++){
+			if (aux->carte.type==1){
+				sprintf(stringtemp, "  |0%d|",aux->carte.valeur);
+			} else if (aux->carte.type==2){
+				sprintf(stringtemp, "  |+2|");
+			} else if (aux->carte.type==3){
+				sprintf(stringtemp, "  |<>|");
+			} else if (aux->carte.type==4){
+				sprintf(stringtemp, "  |=>|");
+			} else if (aux->carte.type==5){
+				sprintf(stringtemp, "  |**|");
+			} else if (aux->carte.type==6){
+				sprintf(stringtemp, "  |+4|");
+			} else {
+				sprintf(stringtemp, "  |00|");
+			}
+			strcat(string,stringtemp);
+			aux = aux->suivant;
+		}
+		printf("%s\n",string);
+	
+		strcpy(string,"");
+		aux = liste;
+		for (y=1;y<i*8+1;y++){
+			aux=aux->suivant;
+		}
+
+		for (y=0;y<8;y++){
+			if (strcmp(aux->carte.couleur,"Ro")==0){
+				strcat(string,"  |Ro|");
+			} else if (strcmp(aux->carte.couleur,"Bl")==0){
+				strcat(string,"  |Bl|");
+			} else if (strcmp(aux->carte.couleur,"Ve")==0){
+				strcat(string,"  |Ve|");
+			} else if (strcmp(aux->carte.couleur,"Ja")==0){
+				strcat(string,"  |Ja|");
+			} else {
+				strcat(string,"  |##|");
+			}
+			aux = aux->suivant;
+		}
+		printf("%s\n",string);
+		printf("  *--*  *--*  *--*  *--*  *--*  *--*  *--*  *--*\n");
+	}
+
+	char string1[56];
+	char string2[56];
+	char string3[56];
+	char string4[56];
+	strcpy(string1,"");
+	strcpy(string2,"");
+	strcpy(string3,"");
+	strcpy(string4,"");
+	
+	//Enfin dans cette boucle on itère sur les dernières cartes restantes si il y en a.
+	//Aux pointe déjà sur la bonne TCelluleCarte.
+	if (aux != NULL){
+		while (aux != NULL) {
+			strcat(string1,"  .--.");
+			if (aux->carte.type==1){
+				sprintf(stringtemp, "  |0%d|",aux->carte.valeur);
+			} else if (aux->carte.type==2){
+				sprintf(stringtemp, "  |+2|");
+			} else if (aux->carte.type==3){
+				sprintf(stringtemp, "  |<>|");
+			} else if (aux->carte.type==4){
+				sprintf(stringtemp, "  |<=|");
+			} else if (aux->carte.type==5){
+				sprintf(stringtemp, "  |JK|");
+			} else if (aux->carte.type==6){
+				sprintf(stringtemp, "  |+4|");
+			} else {
+				sprintf(stringtemp, "  |00|");
+			}
+			strcat(string2,stringtemp);
+			if (strcmp(aux->carte.couleur,"Ro")==0){
+				strcat(string3,"  |Ro|");
+			} else if (strcmp(aux->carte.couleur,"Bl")==0){
+				 strcat(string3,"  |Bl|");
+			} else if (strcmp(aux->carte.couleur,"Ve")==0){
+				strcat(string3,"  |Ve|");
+			} else if (strcmp(aux->carte.couleur,"Ja")==0){
+				strcat(string3,"  |Ja|");
+			} else {
+				strcat(string3,"  |##|");
+			}
+			strcat(string4,"  *--*");
+			aux=aux->suivant;
+		}
+		printf("%s\n",string1);
+		printf("%s\n",string2);
+		printf("%s\n",string3);
+		printf("%s\n\n",string4);
+	}
+}
+
+void melangerPioche(){
+	TCelluleCarte* aux = jeu;
+	TCelluleCarte* prec; 
+	int i =0;
+	int y =0;
+	int select;
+	int tailleFile = 0;
+
+	while (aux != NULL) {
+		tailleFile += 1;
+		aux=aux->suivant;
+	}
+
+	for (i=tailleFile;i>0;i--){
+		select = rand()%i;
+		aux=jeu;
+		prec=jeu;
+		if (select==0) {
+			jeu = aux->suivant;
+			aux->suivant=pioche;
+			pioche=aux;
+		} else {
+			for (y=0;y<select;y++){
+				prec=aux;
+				aux=aux->suivant;
+			}
+			prec->suivant = aux->suivant;
+			aux->suivant = pioche;
+			pioche = aux;
+		}
+	}
+}
+
+void initialiserJeu(){
+	ajoutSerie(1,10,"Bl",1,2);
+	ajoutSerie(1,10,"Ro",1,2);
+	ajoutSerie(1,10,"Ja",1,2);
+	ajoutSerie(1,10,"Ve",1,2);
+
+	ajoutSerie(20,21,"Bl",2,2);
+	ajoutSerie(20,21,"Ro",2,2);
+	ajoutSerie(20,21,"Ja",2,2);
+	ajoutSerie(20,21,"Ve",2,2);
+
+	ajoutSerie(20,21,"Bl",3,2);
+	ajoutSerie(20,21,"Ro",3,2);
+	ajoutSerie(20,21,"Ja",3,2);
+	ajoutSerie(20,21,"Ve",3,2);
+
+	ajoutSerie(20,21,"Bl",4,2);
+	ajoutSerie(20,21,"Ro",4,2);
+	ajoutSerie(20,21,"Ja",4,2);
+	ajoutSerie(20,21,"Ve",4,2);
+
+	ajoutSerie(50,51,"Sa",5,4);
+	ajoutSerie(50,51,"Sa",6,4);
+	ajoutSerie(0,1,"Ve",0,1);
+	ajoutSerie(0,1,"Ve",0,1);
+	ajoutSerie(0,1,"Ve",0,1);
+	ajoutSerie(0,1,"Ve",0,1);
+}
+
+void ajoutSerie(int a, int b, char couleur[], int type, int nb){
+	int i;
+	int y;
+	for (y=0;y<nb;y++){
+		for (i=a;i<b;i++) {
+			TCelluleCarte* newC = (TCelluleCarte*) malloc(sizeof(TCelluleCarte));
+			newC->carte.valeur = i;
+			strcpy(newC->carte.couleur,couleur);
+			newC->carte.type = type;
+			newC->suivant = jeu;
+			jeu = newC;
+		}
+	}
+}
